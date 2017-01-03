b90d8ed942b748deb428db2fa7059768818733a6
codingtherapy <chmoooz@zoho.com>
2017-01-03 23:39:22 +0100

    Create sofa.py

---
+++ sofa.py
@@ -0,0 +1,15 @@
+# some thoughts on "moving sofa problem"
+# https://www.math.ucdavis.edu/~romik/movingsofa/
+# 
+# The main idea is to apply genetic programming.
+# First I generate initial closed polygon contained within "corridor".
+# The figure is small enough to be rotated.
+# Then I succesively modify segments of polygon such that figure is getting bigger and bigger and still can be moved around. 
+
+
+def move(polygon, x, y):
+    return polygon
+
+def rotate(polygon, x0, y0, alpha):
+    return polygon
+    
